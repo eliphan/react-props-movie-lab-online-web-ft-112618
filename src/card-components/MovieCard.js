c41ed8942cd31ca3a9c0fee8a3ffcf3f12c1c1d7
Elise Phan <aliceconnic@gmail.com>
2019-04-10 00:11:53 +0000

    Automatically backed up by Learn

--- src/card-components/MovieCard.js
+++ src/card-components/MovieCard.js
@@ -42,6 +42,8 @@ export default class MovieCard extends Component {
 
 // Don't forget your default props!
 MovieCard.defaultProps = {
-  title: "Unknown"
-  posterSrc: 'default'
+  title: "Unknown",
+  IMDBRating: null,
+  genres: "No Genre(s) Found"
+  posterSrc: "default"
 }
